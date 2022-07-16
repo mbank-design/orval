29c93e021fb5f98d4dd6dc663141b8dc8f67907b
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2022-07-16 20:28:52 +0200

    chore: Rebase

--- src/core/generators/msw.ts
+++ src/core/generators/msw.ts
@@ -9,42 +9,6 @@ import { getRouteMSW } from '../getters/route.msw';
 import { generateDependencyImports } from './imports';
 import { getMockDefinition, getMockOptionsDataOverride } from './mocks';
 
-<<<<<<< HEAD
-=======
-const getRoutePath = (path: string) => {
-  return path.split('').reduce((acc, letter) => {
-    if (letter === '{') {
-      return acc + ':';
-    }
-
-    if (letter === '}') {
-      return acc + '';
-    }
-
-    return acc + sanitize(letter);
-  }, '');
-};
-
-export const getRoute = (route: string, baseUrl = '*') => {
-  const splittedRoute = route.split('/');
-
-  // TODO: Move to configuration object
-  const REMOVE_VERSION_PART = true;
-
-  return (REMOVE_VERSION_PART ? splittedRoute.splice(1, splittedRoute.length) : splittedRoute).reduce((acc, path) => {
-    if (!path) {
-      return acc;
-    }
-
-    if (!path.includes('{')) {
-      return `${acc}/${path}`;
-    }
-
-    return `${acc}/${getRoutePath(path)}`;
-  }, baseUrl);
-};
-
->>>>>>> 8f5bcfd (feat: Allow dates to be deserialized as JavaScript Date objects)
 const MSW_DEPENDENCIES: GeneratorDependency[] = [
   {
     exports: [{ name: 'rest', values: true }],
@@ -115,7 +79,7 @@ export const generateMSW = async (
           ctx.delay(${override?.mock?.delay || 1000}),
           ctx.status(200, 'Mocked status'),${
             value && value !== 'undefined'
-              ? `\nctx.${responseType}(get${pascal(operationId)}Mock()${responseType === 'text' ? '.toString()' : ''}),`
+              ? `\nctx.${responseType}(get${pascal(operationId)}Mock()),`
               : ''
           }
         )
